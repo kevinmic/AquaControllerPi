482a0410739399f5ad5629f82dd5d6c9c3f7d45e
Kevin Dodge <kdodge@alianza.com>
2017-03-04 15:38:34 -0700

    guice is working (I think)

---
+++ rest/src/main/java/com/kevin_mic/aqua/rest/AquaControllerModule.java
@@ -0,0 +1,9 @@
+package com.kevin_mic.aqua.rest;
+
+import com.google.inject.Binder;
+
+public class AquaControllerModule implements com.google.inject.Module {
+    public void configure(Binder binder) {
+
+    }
+}
--- rest/src/main/java/com/kevin_mic/aqua/rest/Main.java
+++ rest/src/main/java/com/kevin_mic/aqua/rest/Main.java
@@ -1,10 +1,13 @@
 package com.kevin_mic.aqua.rest;
 
+import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class Main extends Application<AquaControllerConfig> {
+    private GuiceBundle<AquaControllerConfig> guiceBundle;
+
     public static void main(String[] args) throws Exception {
         new Main().run(args);
     }
@@ -16,12 +19,18 @@ public class Main extends Application<AquaControllerConfig> {
 
     @Override
     public void initialize(Bootstrap<AquaControllerConfig> bootstrap) {
+        guiceBundle = GuiceBundle.<AquaControllerConfig>newBuilder()
+                .addModule(new AquaControllerModule())
+                .setConfigClass(AquaControllerConfig.class)
+                .enableAutoConfig(getClass().getPackage().getName())
+                .build();
+
+        bootstrap.addBundle(guiceBundle);
+
         // nothing to do yet
     }
 
     @Override
     public void run(AquaControllerConfig configuration, Environment environment) {
-        final TestResource resource = new TestResource();
-        environment.jersey().packages(Main.class.getPackage().getName());
     }
 }
