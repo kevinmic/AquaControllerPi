6af8a21c5b5dbd219e777dfdb951a7c57b045e86
Kevin Dodge <kdodge@alianza.com>
2017-03-04 15:45:36 -0700

    Working after adding db config

--- rest/src/main/java/com/kevin_mic/aqua/rest/AquaControllerConfig.java
+++ rest/src/main/java/com/kevin_mic/aqua/rest/AquaControllerConfig.java
@@ -1,6 +1,20 @@
 package com.kevin_mic.aqua.rest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.db.DatabaseConfiguration;
+import io.dropwizard.db.PooledDataSourceFactory;
 
-public class AquaControllerConfig extends Configuration {
+public class AquaControllerConfig extends Configuration implements DatabaseConfiguration {
+    @JsonProperty("database")
+    private DataSourceFactory dataSourceFactory;
+
+    public DataSourceFactory getDataSourceFactory(Configuration configuration) {
+        return null;
+    }
+
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.dataSourceFactory = dataSourceFactory;
+    }
 }
--- rest/src/main/java/com/kevin_mic/aqua/rest/AquaControllerModule.java
+++ rest/src/main/java/com/kevin_mic/aqua/rest/AquaControllerModule.java
@@ -1,9 +1,28 @@
 package com.kevin_mic.aqua.rest;
 
 import com.google.inject.Binder;
+import com.google.inject.Provides;
+import io.dropwizard.jdbi.DBIFactory;
+import io.dropwizard.setup.Environment;
+import org.skife.jdbi.v2.DBI;
 
 public class AquaControllerModule implements com.google.inject.Module {
+    private static DBI jdbi = null;
+
     public void configure(Binder binder) {
 
+    }
+
+    @Provides
+    public DBI get(Environment environment, AquaControllerConfig config) {
+        if (jdbi == null) {
+            synchronized (this) {
+                if (jdbi == null) {
+                    final DBIFactory factory = new DBIFactory();
+                    jdbi = factory.build(environment, config.getDataSourceFactory(null), "postgres");
+                }
+            }
+        }
+        return jdbi;
     }
 }
