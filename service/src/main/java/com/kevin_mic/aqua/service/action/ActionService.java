6fc5567583945bb7b52d6fcdd8feee2803722e58
Kevin Dodge <kdodge@alianza.com>
2017-03-27 22:07:03 -0600

    Adding more methods to action

--- service/src/main/java/com/kevin_mic/aqua/service/action/ActionService.java
+++ service/src/main/java/com/kevin_mic/aqua/service/action/ActionService.java
@@ -4,6 +4,7 @@ import com.kevin_mic.aqua.dao.ActionDao;
 import com.kevin_mic.aqua.model.actions.ActionInterface;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class ActionService {
     private final ActionDao actionDao;
@@ -21,5 +22,21 @@ public class ActionService {
         actionValidator.validateDevices(action);
 
         return (T) actionDao.insert(action);
+    }
+
+    public <T extends ActionInterface> T updateAction(T action) {
+        actionValidator.validate(action);
+        actionValidator.validateRequired(action);
+        actionValidator.validateDevices(action);
+
+        return (T) actionDao.update(action);
+    }
+
+    public List<ActionInterface> listActions() {
+        return actionDao.getActions();
+    }
+
+    public ActionInterface getAction(int actionId) {
+        return actionDao.getAction(actionId);
     }
 }
--- service/src/test/java/com/kevin_mic/aqua/service/action/ActionServiceTest.java
+++ service/src/test/java/com/kevin_mic/aqua/service/action/ActionServiceTest.java
@@ -1,9 +1,13 @@
 package com.kevin_mic.aqua.service.action;
 
 import com.kevin_mic.aqua.dao.ActionDao;
+import com.kevin_mic.aqua.model.actions.ActionInterface;
 import com.kevin_mic.aqua.model.actions.PumpSchedule;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -33,6 +37,36 @@ public class ActionServiceTest {
         verify(actionValidator).validate(action);
         verify(actionValidator).validateRequired(action);
         verify(actionDao).insert(action);
+    }
+
+    @Test
+    public void test_update() {
+        PumpSchedule action = new PumpSchedule();
+        when(actionDao.update(action)).thenReturn(action);
+        assertNotNull(tested.updateAction(action));
+
+        verify(actionValidator).validateDevices(action);
+        verify(actionValidator).validate(action);
+        verify(actionValidator).validateRequired(action);
+        verify(actionDao).update(action);
+    }
+
+    @Test
+    public void test_list() {
+        List<ActionInterface> actions = new ArrayList<>();
+        actions.add(new PumpSchedule());
+        when(actionDao.getActions()).thenReturn(actions);
+
+        List<ActionInterface> actionInterfaces = tested.listActions();
+        assertEquals(actions, actionInterfaces);
+    }
+
+    @Test
+    public void test_getAction() {
+        PumpSchedule action= new PumpSchedule();
+        when(actionDao.getAction(5)).thenReturn(action);
+
+        assertEquals(action, actionDao.getAction(5));
     }
 
 }
